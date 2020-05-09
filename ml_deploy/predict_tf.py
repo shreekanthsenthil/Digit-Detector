8f55d4caa13827558360bb701fe46ef779248a94
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-05-09 22:59:55 +0530

    updated files. Checked output

--- ml_deploy/predict_tf.py
+++ ml_deploy/predict_tf.py
@@ -3,7 +3,7 @@ import cv2
 from tensorflow.keras.models import load_model
 import numpy as np
 import sys
-import json
+# import json
 
 # These lines are written in setup which should be run earlier.
 
@@ -11,9 +11,11 @@ import json
 # MODEL_PATH will not change. But everytime you need to give me the image path.
 # I think we need to use sys.argv[] to take the paths. Do let me know.
 MODEL_PATH = "..\\ml_code\\model.h5"
-IMAGE_PATH = "..\\uploads\\demo.png"
+IMAGE_PATH = "..\\uploads\\imagesimageFileName.png"
 
 def softmax(x):
+    x = x.astype(np.float32)
+    # print(x.dtype)
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0) # only difference
@@ -29,12 +31,18 @@ def predict_image(model_path, img_path):
     img = np.expand_dims(img, axis=0)
     #print(img.shape)
     out = final_model.predict(img)
+    # print(out.dtype)
     # The digit with highest probability
-    return (np.argmax(softmax(out[0])))
+    probability = softmax(out[0])
+    # print(probability)
+    return (np.argmax(probability), np.max(probability))
 
 if __name__ == "__main__":
-    IMAGE_PATH = sys.argv[1]    
-    print(IMAGE_PATH)
-    print("Not over YET")
-    predictions = predict_image(MODEL_PATH, IMAGE_PATH)
-    print(predictions)
+    # IMAGE_PATH = sys.argv[1]    
+    # print(IMAGE_PATH)
+    # print("Not over YET")
+    print("-----------------Running MODEL----------------")
+    prediction, probability = predict_image(MODEL_PATH, IMAGE_PATH)
+    print("Confidence: %0.4f" %(probability))
+    print("Prediction: %d" %(prediction))
+    print("----------Done--------------------------------")
